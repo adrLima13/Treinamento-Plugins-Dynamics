01e8dd5a27476942f415cf2bc6168fd9c9bb4901
adrLima13 <adriano.albuquerque@outlook.com>
2025-05-19 01:11:11 -0300

    Meus primeiros plugins Dynamics

---
+++ PluginAccountPreOperation.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+
+namespace PluginsTreinamento
+{
+    public class PluginAccountPreOperation : IPlugin
+    {
+        // método requerido para execução do Plugin recebendo como parâmetro os dados do provedor de serviço
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            // Variável contendo o contexto da execução
+            IPluginExecutionContext context =
+                (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            // Variável contendo o Service Factory da Organização
+            IOrganizationServiceFactory serviceFactory =
+                (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+
+            // Variável contendo o Service Admin que estabele os serviços de conexão com o Dataverse
+            IOrganizationService serviceAdmin = serviceFactory.CreateOrganizationService(null);
+
+            // Variável do Trace que armazena informações de LOG
+            ITracingService trace = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            // Verifica se contém dados para o destino e se corresponde a uma Entity
+            if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+            {
+                // Variável do tipo Entity herdando a entidade do contexto
+                Entity entidadeContexto = (Entity)context.InputParameters["Target"];
+
+                if (entidadeContexto.LogicalName == "account") // verifica se a entidade do contexto é account
+                {
+                    if (entidadeContexto.Attributes.Contains("telephone1")) // verifica se contém o atributo telephone1
+                    {
+                        // variavel para herdar o conteudo do atributo telephonel do contexto
+                        var phone1 = entidadeContexto["telephone1"].ToString();
+
+                        // variavel string contendo FetchXML para consulta do contato
+                        string FetchContact = @"<?xml version='1.0'?>" +
+                        "<fetch distinct='false' mapping='logical' output-format='xml-platform' version='1.0'>" +
+                        "<entity name='contact'>" +
+                        "<attribute name='fullname'/>" +
+                        "<attribute name='telephone1'/>" +
+                        "<attribute name='contactid'/>" +
+                        "<order descending='false' attribute='fullname'/>" +
+                        "<filter type='and'>" +
+                        "<condition attribute='telephone1' value='" + phone1 + "' operator='eq'/>" +
+                        "</filter>" +
+                        "</entity>" +
+                        "</fetch>";
+
+                        trace.Trace("FetchContact: " + FetchContact); // Armazena informacoes de LOG
+
+                        // variável contendo o retorno da consulta FetchXML
+                        var primarycontact = serviceAdmin.RetrieveMultiple(new FetchExpression(FetchContact));
+
+                        if (primarycontact.Entities.Count > 0) // verifica se contém entidade
+                        {
+                            // para cada entidade retornada atribui a variável entityContact
+                            foreach (var entityContact in primarycontact.Entities)
+                            {
+                                //atribui referencia de entidade para o atributo primarycontactid (Contato Primario)
+                                entidadeContexto["primarycontactid"] = new EntityReference("contact", entityContact.Id);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ PluginAccountPreValidation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+
+
+namespace PluginsTreinamento
+{
+    public class PluginAccountPreValidation : IPlugin
+    {
+        // método requerido para execuçao do Plugin recebendo como parametro os dados do provedor de serviço
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            // Variavel contendo o contexto da execuçao
+            IPluginExecutionContext context =
+                (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            // Variável contendo o Service Factory da Organizaçao
+            IOrganizationServiceFactory serviceFactory =
+                (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+
+            // Variavel contendo o Service Admin que estabele os servicos de conexao com o Dataverse
+            IOrganizationService serviceAdmin = serviceFactory.CreateOrganizationService(null);
+
+            // Variavel do Trace que armazena informações de LOG
+            ITracingService trace = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            // Variável do tipo Entity vazia
+            Entity entidadeContexto = null;
+
+            if (context.InputParameters.Contains("Target")) // Verifica se contém dados para o destino
+            {
+                entidadeContexto = (Entity)context.InputParameters["Target"]; // atribui o contexto da entidade para a variavel
+
+                trace.Trace("Entidade do Contexto: " + entidadeContexto.Attributes.Count); // armazena informaçoes de LOG
+
+                if (entidadeContexto == null) //verifica se a entidade do contexto está vazia
+                {
+                    return; // caso verdadeira retorna sem nada executar
+                }
+
+                if (!entidadeContexto.Contains("telephone1")) // verifica se o atributo telephonel não está presente no contexto
+                {
+                    throw new InvalidPluginExecutionException("Campo Telefone principal é obrigatório!"); // exibe Exception de Erro
+                }
+            }
+        }
+    }
+}
