a8912cfcddb0e629b7bc97027220b63223ad00fc
adrLima13 <adriano.albuquerque@outlook.com>
2025-05-23 02:09:50 -0300

    Trabalhando com Action e WF Assembly

---
+++ ActionBuscaCEP.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using Microsoft.Xrm.Sdk;
+
+namespace PluginsTreinamento
+{
+    public class ActionBuscaCEP : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context =
+                    (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            IOrganizationServiceFactory serviceFactory =
+                (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+
+            IOrganizationService serviceAdmin = serviceFactory.CreateOrganizationService(null);
+
+            ITracingService trace = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            var cep = context.InputParameters["CepInput"];
+            trace.Trace("Cep informado: " + cep);
+
+            var viaCEPUrl = $"https://viacep.com.br/ws/{cep}/json/";
+            string result = string.Empty;
+            using (WebClient client = new WebClient())
+            {
+                client.Headers[HttpRequestHeader.ContentType] = "application/json";
+                client.Encoding = Encoding.UTF8;
+                result = client.DownloadString(viaCEPUrl);
+            }
+            context.OutputParameters["ResultadoCEP"] = result;
+
+            trace.Trace("Resultado: " + result);
+        }
+    }
+}
---
+++ MinhaPrimeiraAction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+
+namespace PluginsTreinamento
+{
+    public class MinhaPrimeiraAction : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context =
+                (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            IOrganizationServiceFactory serviceFactory =
+                (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+
+            IOrganizationService serviceAdmin = serviceFactory.CreateOrganizationService(null);
+
+            ITracingService trace = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            trace.Trace("Minha Primeira Action executada com sucesso!");
+
+            Entity entityAluno = new Entity("dio_alunos");
+            entityAluno["dio_name"] = "Adriano de Albuquerque Lima";
+            entityAluno["dio_cpf"] = "111.222.333-44";
+            entityAluno["ownerid"] = new EntityReference("systemuser", context.UserId);
+            Guid guidAluno = serviceAdmin.Create(entityAluno);
+            trace.Trace("Aluno criado: " + guidAluno);
+        }
+    }
+}
